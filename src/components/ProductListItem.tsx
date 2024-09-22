eef7c5afeb0a3762338e26c2006471395d8f20bc
kaustin <krystalaustin99@gmail.com>
2024-09-22 16:55:31 -0500

    create FlatList, connect the product data

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,6 @@
 import { View, Text, Image, StyleSheet } from 'react-native';
-import { Colors } from '../constants/Colors';
-import { Product } from '../types';
+import { Colors } from '@/src/constants/Colors';
+import { Product } from '@/src/types';
 
 type ProductListItemProps = {
   product: Product;
@@ -12,7 +12,6 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
       <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image} />
-      
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -23,9 +22,15 @@ export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    maxWidth: '50%', //should equal 100 divided by numOfColums(in FlatList inside index)
     backgroundColor: '#fff',
     padding: 10, 
     borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
   },
   title: {
     fontSize: 18,
@@ -36,8 +41,4 @@ const styles = StyleSheet.create({
     color: Colors.light.tint,
     fontWeight: 'bold',
   },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  }
 });
