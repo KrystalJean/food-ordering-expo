72202da4e8f075b5554a29eab20df533b8139e11
kaustin <krystalaustin99@gmail.com>
2024-09-22 13:04:27 -0500

    created ProductListItem component

--- src/app/(tabs)/explore.tsx
+++ src/app/(tabs)/explore.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 18,
-    fontWeight: 600,
+    fontWeight: '600',
     marginVertical: 10,
   },
   price: {
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,40 +1,12 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { Colors } from '../../constants/Colors';
+import { View } from 'react-native';
 import products from '../../../assets/data/products';
-
-const product = products[1];
+import ProductListItem from '@/src/components/ProductListItem';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image}} style={styles.image} />
-      
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    padding: 10, 
-    borderRadius: 20,
-    // flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: 600,
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  }
-});
